26406792e1441d4345dfb46d742133d258e761fd
AlphaCharlieNexus007 <corpowork007@gmail.com>
2026-04-08 19:37:08 +0530

    Delete BACKEND directory

--- BACKEND/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SESSION_SECRET", "clout-dev-secret-change-me")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///clout.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET = os.environ.get("SESSION_SECRET", "clout-dev-secret-change-me")
